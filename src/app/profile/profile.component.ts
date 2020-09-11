306c424302d77d11b8bf57a7f4e76af0d0f96055
MohamedAdel99 <mohamed.3adel11@gmail.com>
2020-09-11 19:31:40 +0200

    updating profile view

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -4,6 +4,7 @@ import {LocalStorageService} from 'ngx-webstorage';
 import { OrderService } from '../order.service';
 import { OrderPl } from '../checkout/orderpl';
 import { Observable } from 'rxjs';
+import {ProfileClass} from './profileclass'
 
 @Component({
   selector: 'app-profile',
@@ -15,15 +16,21 @@ export class ProfileComponent implements OnInit {
   s=''
   x='active'
   username=''
-  user:any
+  user : ProfileClass = {};
+  id: number;
   orders:Observable<Array<OrderPl>>
+
   constructor(private authservice:AuthService , private localStoraqeService: LocalStorageService,private orderservice:OrderService) { }
 
   ngOnInit(): void {
     this.username=this.localStoraqeService.retrieve('username');
     let obs = this.authservice.getuserbyun(this.username);
-    obs.subscribe((data) => this.user=data);
-    this.orders = this.orderservice.getordersbyid(this.user.cid);
+    obs.subscribe(data => {
+      this.user = data;
+      this.orders = this.orderservice.getordersbyid(this.user.cid);
+ },
+ error => console.log(error)
+);
   }
  raise(){
     this.flag=1;
---
+++ src/app/profile/profileclass.ts
@@ -0,0 +1,8 @@
+export class ProfileClass {
+    cid?:number;
+    username?: String;
+    email?: String;
+    phonenum?: String;
+    address?: String;
+    payment?: String;
+  }
